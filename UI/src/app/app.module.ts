16338d29e59f969df0ad895e8ca2f81df56b8004
himajaM <himaja@sails.solutions>
2022-01-18 16:16:37 +0530

    Initial Angular setup

---
+++ UI/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './common-components/login/login.component';
+import { ForgotPasswordComponent } from './common-components/forgot-password/forgot-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
